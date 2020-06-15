7698066f1631afbd7c75d47df426e7a2a0189f4c
alfatektecnologia <oliveiraemanoel.br@gmail.com>
2020-06-15 12:39:18 -0300

    150620

--- app/src/main/java/br/com/oliveiraemanoel/urestaurant/views/CardapioActivity.java
+++ app/src/main/java/br/com/oliveiraemanoel/urestaurant/views/CardapioActivity.java
@@ -7,8 +7,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +23,7 @@ import br.com.oliveiraemanoel.urestaurant.adapters.MenuGrupoAdapter;
 import br.com.oliveiraemanoel.urestaurant.adapters.MenuItemAdapter;
 import br.com.oliveiraemanoel.urestaurant.models.Item;
 import br.com.oliveiraemanoel.urestaurant.models.UMenu;
+import br.com.oliveiraemanoel.urestaurant.util.CountDrawable;
 import br.com.oliveiraemanoel.urestaurant.viewmodel.MenuViewModel;
 
 public class CardapioActivity extends AppCompatActivity {
@@ -49,6 +55,7 @@ public class CardapioActivity extends AppCompatActivity {
 
         itemRecycler.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
 
+        //accessing viewmodel
         MenuViewModel menuViewModel = ViewModelProviders.of(this).get(MenuViewModel.class);
 
         menuViewModel.getMenu().observe(this, new Observer<List<UMenu>>() {
@@ -78,16 +85,12 @@ public class CardapioActivity extends AppCompatActivity {
                 if(groupItemList.size()>0){
                     updateAdapter(true);
                 }
-
-
             }
-
         });
 
-
-
-
     }
+    //this method update itemAdapter with the new list from group selected by user in
+    //menugroupadapter
     public static void updateAdapter(boolean adapter){
         if(adapter)
         { //setting group 0(burguers) as default to show items
@@ -97,9 +100,37 @@ public class CardapioActivity extends AppCompatActivity {
                 itemRecycler.setAdapter(itemAdapter);
                 itemAdapter.notifyDataSetChanged();
 
-
             }
 
     }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.cart_action_bar, menu);
+      // setCount(this, "9");
+        return true;
+
+    }
+
+    public void setCount(Context context, String count) {
+        Menu menu = findViewById(R.id.cartAction);
+        MenuItem menuItem = menu.findItem(R.id.cartIcom);
+        LayerDrawable icon = (LayerDrawable) menuItem.getIcon();
+
+        CountDrawable badge;
+
+        // Reuse drawable if possible
+        Drawable reuse = icon.findDrawableByLayerId(R.id.cartCount);
+        if (reuse != null && reuse instanceof CountDrawable) {
+            badge = (CountDrawable) reuse;
+        } else {
+            badge = new CountDrawable(context);
+        }
+
+        badge.setCount(count);
+        icon.mutate();
+        icon.setDrawableByLayerId(R.id.cartCount, badge);
+    }
+
 
 }
